ba34030f5f29686396ff0e028c34d0054d0271c1
fHuteau <57004056+fHuteau@users.noreply.github.com>
2026-06-12 13:21:20 +0200

    Update CrowdSec module imports to new repository

--- imports.go
+++ imports.go
@@ -4,8 +4,8 @@ import (
 	// Import the default CrowdSec modules. Primary reason this
 	// file exists is to satisfy the Caddy documentation and download
 	// pages to list the modules correctly.
-	_ "github.com/hslatman/caddy-crowdsec-bouncer/appsec"
-	_ "github.com/hslatman/caddy-crowdsec-bouncer/crowdsec"
-	_ "github.com/hslatman/caddy-crowdsec-bouncer/http"
-	_ "github.com/hslatman/caddy-crowdsec-bouncer/layer4"
+	_ "github.com/fhuteau/caddy-crowdsec-bouncer/appsec"
+	_ "github.com/fhuteau/caddy-crowdsec-bouncer/crowdsec"
+	_ "github.com/fhuteau/caddy-crowdsec-bouncer/http"
+	_ "github.com/fhuteau/caddy-crowdsec-bouncer/layer4"
 )
